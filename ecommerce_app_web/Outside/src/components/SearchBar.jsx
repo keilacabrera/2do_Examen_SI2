0a6226aa4dc0208fc0a6002a8e302bca96325aa9
keilacabrera <keilacabrera63@gmail.com>
2025-11-01 12:48:34 -0400

    change search

--- ecommerce_app_web/Outside/src/components/SearchBar.jsx
+++ ecommerce_app_web/Outside/src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ export default function SearchBar({ search, setSearch, setPage }) {
         type="text"
         value={search}
         onChange={handleChange}
-        placeholder="Buscar categoría..."
+        placeholder="Buscar ..."
         className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
     </div>
