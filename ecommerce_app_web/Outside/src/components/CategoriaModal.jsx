27576958a4baafae0ade240c8f823a1bb86e206b
keilacabrera <keilacabrera63@gmail.com>
2025-11-05 00:26:32 -0400

    update category

--- ecommerce_app_web/Outside/src/components/CategoriaModal.jsx
+++ ecommerce_app_web/Outside/src/components/CategoriaModal.jsx
@@ -42,18 +42,6 @@ export default function CategoriaModal({ isOpen, onClose, onSave, categoriaActua
             />
           </div>
 
-          {/* <div>
-            <label className="block text-sm font-medium text-gray-600 mb-1">
-              Descripción
-            </label>
-            <textarea
-              value={descripcion}
-              onChange={(e) => setDescripcion(e.target.value)}
-              rows={3}
-              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            ></textarea>
-          </div> */}
-
           <div className="flex justify-end gap-3 pt-3">
             <button
               type="button"
